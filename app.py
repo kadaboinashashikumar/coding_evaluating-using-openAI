93d50338fcae3e50524fe42c4db1fe473d810d84
kadaboinashashikumar <62072375+kadaboinashashikumar@users.noreply.github.com>
2024-08-08 12:35:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,27 +13,6 @@ openai.api_key = api_key
 
 
 
-#@title generating question
-def generate_problem_statement(input_text):
-    # Prompt for GPT-3
-  prompt = input_text
-
-    # Parameters for GPT-3 completion
-  response = openai.chat.completions.create(
-        model="gpt-4o-mini-2024-07-18",  # Use a chat model like gpt-3.5-turbo
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt}
-        ],
-        max_tokens=4000,
-        temperature=0.5# Adjust max_tokens based on desired response length
-    )
-
-    # Extract and return feedback from GPT-3 response
-  feedback = response.choices[0].message.content.strip()
-  return feedback
-
-
 #@title evaluating code
 def evaluate_code(code): 
   prompt = f"""evaluate the code:
@@ -58,19 +37,6 @@ def evaluate_code(code):
 
 
 
-#optional_output
-
-"""query=f"Generate a programming problem suitable for a coding test with test cases, time complexity and can be solved in any programming language "
-coding_question=generate_problem_statement(query)
-print(coding_question)
-
-
-user_solution=input("enter the solution")
-
-feedback = evaluate_code(user_solution) 
-print(feedback)"""
-
-
 
 #@title Flask app
 from markupsafe import escape
@@ -81,16 +47,13 @@ app = Flask(__name__, template_folder='/content/drive/MyDrive/templates')
 
 @app.route('/')
 def index():
-    global coding_question
-    query=f"Generate a programming problem suitable for a coding test with test cases, time complexity and can be solved in any programming language "
-    coding_question=generate_problem_statement(query)
-    c=escape(coding_question)
+     
+    c="This a coding test, Enter the solution below"
     return render_template('index.html', question=c)
 
 @app.route('/submit', methods=['POST'])
 def submit():
     user_solution = request.form['solution']
-    #feedback = evaluate_code(coding_question,user_solution)
     feedback = evaluate_code(user_solution)
     f=escape(feedback)
 
